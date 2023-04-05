cd1c93c20402d700a561d4ad02070d2c573d6b7d
GurpreetSarangal <gurpreetsarangal7@gmail.com>
2023-04-05 11:45:56 +0530

    models created

--- afraas_server/reports/models.py
+++ afraas_server/reports/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from user.models import *
 
 # Create your models here.
+class Attendance(models.Model):
+    time_stamp = models.TimeField()
+    STATUS_CHOICES = [
+    ("enter", "enter"),
+    ("exit", "exit"),
+    ("absent", "absent"),
+    ]
+    status = models.CharField(default="enter", max_length=50, choices=STATUS_CHOICES)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- afraas_server/user/models.py
+++ afraas_server/user/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Shift(models.Model):
+    time_in = models.TimeField()
+    time_out = models.TimeField()
+
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    shift = models.ForeignKey(Shift, on_delete=models.CASCADE)
+
+
